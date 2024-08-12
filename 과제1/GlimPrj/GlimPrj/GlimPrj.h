2d55f1234fe26aba6cbb29d85c3289240d9c1853
24onsi <24onsi@gmail.com>
2024-08-13 04:48:22 +0900

    과제1

---
+++ 과제1/GlimPrj/GlimPrj/GlimPrj.h
@@ -0,0 +1,32 @@
+﻿
+// GlimPrj.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CGlimPrjApp:
+// 이 클래스의 구현에 대해서는 GlimPrj.cpp을(를) 참조하세요.
+//
+
+class CGlimPrjApp : public CWinApp
+{
+public:
+	CGlimPrjApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGlimPrjApp theApp;
